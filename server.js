bc6884ec9961ebc48ba79fc4afb23decab3997e3
Neelesh Shetty <neelesh@pichainlabs.com>
2021-10-10 17:33:54 +0530

    data added

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+console.log(
+    `\{^_^}/ hi!
+
+\x1b[36m%s\x1b[0m`, `server running at port ${port}...`
+)
